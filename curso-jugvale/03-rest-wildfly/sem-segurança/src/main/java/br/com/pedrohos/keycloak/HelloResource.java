68aa14015c3f1443b437b7bfc57a54b49a8f0af7
pedro-hos <pedro-hos@outlook.com>
2019-08-01 19:27:17 -0300

    sem agora

---
+++ curso-jugvale/03-rest-wildfly/sem-segurança/src/main/java/br/com/pedrohos/keycloak/HelloResource.java
@@ -0,0 +1,33 @@
+package br.com.pedrohos.keycloak;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class HelloResource {
+
+	@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("public")
+    public Response getPublic(){
+        return Response.ok().entity(new Message("public")).header("Access-Control-Allow-Origin", "*").build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("secured")
+    public Response getSecured() {
+    	return Response.ok().entity(new Message("secured")).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("admin")
+    public Response getAdmin() {
+    	return Response.ok().entity(new Message("admin")).build();
+}
+
+}
