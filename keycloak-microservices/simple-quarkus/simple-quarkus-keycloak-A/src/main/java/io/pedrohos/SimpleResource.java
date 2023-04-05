c00090d028b90f6f2776e274593137954d5d6e22
Pedro Hos <pedro-hos@outlook.com>
2023-04-05 16:19:08 -0300

    PEP example

---
+++ keycloak-microservices/simple-quarkus/simple-quarkus-keycloak-A/src/main/java/io/pedrohos/SimpleResource.java
@@ -0,0 +1,37 @@
+package io.pedrohos;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.annotations.cache.NoCache;
+
+import io.quarkus.security.identity.SecurityIdentity;
+
+/**
+ * @author Pedro Hos <pedro-hos@outlook.com>
+ *
+ */
+@Path("/api")
+@Produces("text/plain")
+public class SimpleResource {
+    
+    @Inject
+    SecurityIdentity identity;
+
+    @GET
+    @Path("hello")
+    public Response hello() {
+        return Response.ok("Hello app A").build();
+    }
+    
+    @GET
+    @Path("/me")
+    @NoCache
+    public Response me() {
+        return Response.ok(identity.getPrincipal().getName()).build();
+    }
+
+}
